e2985ee8432d0df0c5311c2520c0598d14a9f58f
noturbob <bobbyanthenrao@gmail.com>
2025-09-18 14:04:11 +0530

    Test Complete

--- src/components/tourist/home/components/Header.tsx
+++ src/components/tourist/home/components/Header.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import NotificationsRoundedIcon from '@mui/icons-material/NotificationsRounded';
-import CustomDatePicker from './CustomDatePicker';
 import NavbarBreadcrumbs from './NavbarBreadcrumbs';
 import MenuButton from './MenuButton';
 import ColorModeIconDropdown from '../../../theme/ColorModelIconDropdown';
@@ -25,7 +24,6 @@ export default function Header() {
       <NavbarBreadcrumbs />
       <Stack direction="row" sx={{ gap: 1 }}>
         <Search />
-        <CustomDatePicker />
         <MenuButton showBadge aria-label="Open notifications">
           <NotificationsRoundedIcon />
         </MenuButton>
